8d857c14485c2b035fe717faf247cb10725112d9
ReddSnake <horatiu.crisan01@e-uvt.ro>
2024-03-11 19:08:30 +0200

    Create a subset
    A subset allows multiple tests to be in a group
    A subset also describes different scenarios

--- hello.go
+++ hello.go
@@ -4,8 +4,13 @@ import (
 	"fmt"
 )
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string { /* keeping our domain from the outside world (side-effects) */
-	return "Hello, " + name + "!"
+	if name == "" {
+		name = "world"
+	}
+	return englishHelloPrefix + name + "!"
 }
 
 func main() { /* The fmt.Println() function is a side effect since it prints to the stdout */
--- hello_test.go
+++ hello_test.go
@@ -11,4 +11,18 @@ func TestHello(t *testing.T) { /* Running a test to check if the returning value
 	if got != want { /* If the result is different, we are printing the error message */
 		t.Errorf("got %v, want %v", got, want)
 	}
+
+	/*
+		Creating a failing function
+		This is a subset of tests that allow tests to be groupped around "a thing"
+		A subset describes different scenarios
+	*/
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world!"
+
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
 }
